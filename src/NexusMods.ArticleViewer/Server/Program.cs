0cea35e034be08394367bba1b277b2d7cafb493e
Aragas <personal@aragas.org>
2022-01-29 13:50:33 +0300

    Fix

--- src/NexusMods.ArticleViewer/Server/Program.cs
+++ src/NexusMods.ArticleViewer/Server/Program.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
+using System;
 using System.Threading.Tasks;
 
 namespace NexusMods.ArticleViewer.Server
 {
     public class Program
     {
-        public static Task Main(string[] args) => CreateHostBuilder(args).Build().RunAsync();
+        public static Task Main(string[] args)
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+            return CreateHostBuilder(args).Build().RunAsync();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) => Host
             .CreateDefaultBuilder(args)
